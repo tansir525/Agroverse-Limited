bf0af9f1a1b7630e57832023593ef1655c967b12
Hussain K M Tansire <tansirorick525@gmail.com>
2022-01-22 09:00:31 +0800

    Nav and Banner updated

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Home',
+  },
+  {
+    path: 'services',
+    label: 'About',
+  },
+  {
+    path: 'testimonials',
+    label: 'Career',
+  },
+  {
+    path: 'news',
+    label: 'Contact Us',
+  },
+];
